e4e3ffc27d425a37dc20123039637b4fb58e0f02
JeremyLiew <jeremyliew@etctech.com.my>
2024-12-26 13:46:22 +0800

    .

--- vuejs/vue-admin/modules/_general/router.ts
+++
@@ -1,5 +0,0 @@
-const Error404 = () => import( './views/404.vue')
-
-export {
-	Error404,
-}
--- vuejs/vue-admin/modules/auth/router.ts
+++
@@ -1,9 +0,0 @@
-const ForgotPassword = () => import('./views/ForgotPassword.vue')
-const Login = () => import( './views/Login.vue')
-const ResetPassword = () => import('./views/ResetPassword.vue')
-
-export {
-	ForgotPassword,
-	Login,
-	ResetPassword,
-}
